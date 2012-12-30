7b805fad3570606278c58ff61bc93b7d9690f7b8
Daniel Nouri <daniel.nouri@gmail.com>
2012-12-30 19:20:26 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 
 setup(name='gdbn',
-      version='0.1',
+      version='0.2dev',
       description="Pre-trained deep neural networks",
       long_description=README,
       author='George Dahl',
